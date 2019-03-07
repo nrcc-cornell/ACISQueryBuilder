d8effbe24c2cff03a8e5eafa621115ebd01fae30
KEggleston <kle1@cornell.edu>
2019-03-07 15:35:58 -0500

    Moved over version 2 built with reaact

---
+++ precache-manifest.a5b54c1dd5414f71b967c9d0afb58e04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7246118cf19d0d874e18",
+    "url": "/static/js/main.7246118c.chunk.js"
+  },
+  {
+    "revision": "142b14cea9e998d6cada",
+    "url": "/static/js/1.142b14ce.chunk.js"
+  },
+  {
+    "revision": "5714efeb25dfd7d441633e36eb59f75c",
+    "url": "/index.html"
+  }
+];
