a7dc43e76cbb3643ec80468dfba0187b6b9fadcf
KEggleston <kle1@cornell.edu>
2020-01-21 14:13:58 -0500

    Replacement button and network functionality

---
+++ precache-manifest.712b2e93798eafd1309eba15a086c6d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "98be22731a84f3cbfd2f",
+    "url": "/static/js/main.98be2273.chunk.js"
+  },
+  {
+    "revision": "260a5dd873d3f37aa817",
+    "url": "/static/js/1.260a5dd8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed32c1d696617d8e3d7504ecaf4592fd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cf5aee9962db5631c0c2f2880c4c93d.js"
+  "/precache-manifest.712b2e93798eafd1309eba15a086c6d3.js"
 );
 
 workbox.clientsClaim();
